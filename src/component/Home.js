94104f3986b640de39e6444df3f192992c4dc325
spelzi <60978130+spelzi@users.noreply.github.com>
2024-08-03 05:21:26 +0100

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "../component/style.css";
+import Popups from "./Popups";
+import { useState } from "react";
+
+const Home = () => {
+  const [buttonPopup, setButtonPopup] = useState(false);
+  return (
+    <div className="home">
+      <header className="header">
+        <h1>
+          🌺🌺TO MY ONE AND ONLY SPECIAL ONE🌺🌺
+          <br />
+          <br /> 🌺🌺UGOCHI🌺🌺
+        </h1>
+      </header>
+      <body>
+        <div className="h2">
+          <h2>CLICK HERE</h2>
+        </div>
+        <div className="icon">
+          <i class="fa-solid fa-arrow-down"></i>
+        </div>
+        <div className="button">
+          <button onClick={() => setButtonPopup(true)}>Click Me</button>
+          <Popups trigger={buttonPopup} setTrigger={setButtonPopup}>
+            <h2>TO MY WORLD CUTEST </h2>
+            <p>
+              My baby girl for lifE, My cutie goddess have a Wonderful
+              day😘😘😘😘
+            </p>
+          </Popups>
+        </div>
+      </body>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Home;
